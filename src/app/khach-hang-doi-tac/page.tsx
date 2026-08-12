8dd21035ca811007e6592ce081464a6cec8d2c2e
lenguyenhoang123 <lenguyenhoang9.10@gmail.com>
2026-08-12 09:59:59 +0700

    feat: add UI for partners and customers page

---
+++ src/app/khach-hang-doi-tac/page.tsx
@@ -0,0 +1,13 @@
+import { constructMetadata } from "@/lib/seo";
+import CustomersPartnersView from "./_views/customers-partners-view";
+
+export const metadata = constructMetadata({
+  title: "Khách hàng & Đối tác",
+  description:
+    "Khách hàng tiêu biểu và mạng lưới đối tác chiến lược của Kepler Group.",
+  url: "/khach-hang-doi-tac",
+});
+
+export default function KhachHangDoiTacPage() {
+  return <CustomersPartnersView />;
+}
